02af5590e9b1f18d10b508c46272a608287c0d59
anon <sarah.anne.friend@gmail.com>
2020-04-26 10:34:42 -0400

    Remove boilerplate

---
+++ src/components/App.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react'
+import { Route, Switch } from 'react-router-dom';
+import './../index.css';
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  handleConsent(int) {
+    // this.setState({ stage: int })
+  }
+
+  handleArchiveId(id) {
+    // console.log('setting archiveId')
+    // this.setState({ archiveId: id })
+  }
+
+  render() {
+    return //(
+    // <div className="content">
+    //   <h2>remembering network</h2>
+    //   { this.state.stage === 0 ?
+    //     <ConsentForm handleConsent={this.handleConsent.bind(this)} /> :
+    //     null }
+    //   { this.state.stage === 1 ?
+    //     <ImageForm
+    //       handleConsent={this.handleConsent.bind(this)}
+    //       handleArchiveId={this.handleArchiveId.bind(this)} /> :
+    //     null }
+    //   { this.state.stage === 2 ?
+    //     <ThankYou
+    //       handleConsent={this.handleConsent.bind(this)}
+    //       archiveId={this.state.archiveId} /> :
+    //     null }
+    // </div>
+    // )
+  }
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Home} />
+{/*          <Route path="/display" component={Display} />*/}
+        </Switch>
+      </div>
+    )
+  }
+}
+
+export default App;
