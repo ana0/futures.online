88493e56bd173afac6ca24e3b3b0fd9c4602e560
anon <sarah.anne.friend@gmail.com>
2020-04-28 19:37:25 -0400

    Clean up dropdown

--- src/components/base/Dropdown.jsx
+++ src/components/base/Dropdown.jsx
@@ -9,12 +9,6 @@ export default class Dropdown extends Component {
   } 
 
   baseStyle = {
-    height: this.props.height || '600px',
-    width: this.props.width || '50%',
-    left: this.props.left || '0px',
-    right: this.props.right || '0px',
-    top: this.props.top || '50px',
-    margin: this.props.margin
   }
 
   handleClose() {
